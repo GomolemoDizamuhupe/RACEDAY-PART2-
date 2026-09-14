51220bd3ca14f64126fcd3fc2b5a66861a9f9d6d
GomolemoDizamuhupe <ST10491441@rcconnect.edu.za>
2026-09-14 22:15:58 +0200

    Updated ApplicationDBContext.cs

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RACEDAY_PART2_.Models;
 
 namespace RACEDAY_PART2_.Data
 {
@@ -6,5 +7,14 @@ namespace RACEDAY_PART2_.Data
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
         { }
+
+        public DbSet<Users> users { get; set; }
+        public DbSet<Results> results { get; set; }
+        public DbSet<RaceRoutes> raceRoutes { get; set; }
+        public DbSet<Events> events { get; set; }
+        public DbSet<Entries> entries { get; set; }
+        public DbSet<Categories> categories { get; set; }
+
+
     }
 }
